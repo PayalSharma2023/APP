f38baa7920adb39203d0f17e3ae351613771fa07
Payal Sharma <payalsharmareal@gmail.com>
2024-05-28 11:51:55 +0530

    firebase setup

--- twitter_clone/lib/main.dart
+++ twitter_clone/lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:twitter_clone/SignUp.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+void main() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  
   const MyApp({super.key});
 
   // This widget is the root of your application.
